7635bcc2e80fba910b679b233ddd3389eb2ff05c
Dennis M <dennismarkshome@gmail.com>
2020-03-22 17:56:57 -0400

    Fix scrollable navbar (issue #21)

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import { NavDropdown, Navbar, Nav } from "react-bootstrap"
+import { NavDropdown, Navbar, Nav } from "react-bootstrap";
 
 export default class Main extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     console.log("initialized");
   }
@@ -10,7 +10,13 @@ export default class Main extends Component {
   render() {
     return (
       <div className="app-frame">
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="dark"
+          variant="dark"
+          style={{ position: "fixed", width: "100vw", height: "50px" }}
+        >
           <Navbar.Brand href="#home">SDCTools</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
@@ -20,10 +26,16 @@ export default class Main extends Component {
               <Nav.Link href="/patitents">Patients</Nav.Link>
               <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
               </NavDropdown>
             </Nav>
             <Nav>
