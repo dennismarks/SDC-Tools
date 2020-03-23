209614c7f84d7b3d3f8e4bcfa1670c4b9f121f90
Max Pham <max.pham@mail.utoronto.ca>
2020-03-23 14:57:04 -0400

    WIP: partial form

--- backend/models/form.model.js
+++ backend/models/form.model.js
@@ -1,31 +1,55 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const FormSchema = new Schema(
-  {
-    patient_number: {
-      type: Number,
-      required: true,
-      unique: true
-    },
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 3
-    },
-    email: {
-      type: String,
-      required: true
-    },
-    phone: {
-      type: Number,
-      required: true
-    }
+const AnswerSchema = new Schema({
+  questionID: {
+    type: Number,
+    required: true,
+    unique: true
   },
-  {
-    timestamps: true
-  }
-);
+  answer: String // All strings can be convert to option.
+});
+
+const QuestionSchema = new Schema({
+  questionID: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  questionTitle: String,
+  enableState: Boolean,
+  dependentQuestions: [QuestionSchema],
+  answerType: {
+    type: Number // 0 - Text (or Integer); 1 - MultipleChoice; 2 - T/F
+  },
+  answer: AnswerSchema
+});
+
+const SectionSchema = new Schema({
+  sectionID: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  sectionTitle: {
+    type: String,
+    required: true
+  },
+  questions: [QuestionSchema]
+});
+
+const FormSchema = new Schema({
+  formID: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  diagnostic_id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  sections: [SectionSchema]
+});
 
 module.exports = mongoose.model("form", FormSchema);
