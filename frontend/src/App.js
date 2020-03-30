f68145fb8c3fba8df9cb3998179375289244997a
Max Pham <max.pham@mail.utoronto.ca>
2020-03-30 14:55:54 -0400

    Revert changes - Please make PR

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Main from "./components/Main"
+import Main from "./components/Main";
 import HomePage from "./views/HomePage";
 import FormPage from "./views/FormPage";
+import PatientPage from "./views/PatientPage";
 import NotFoundPage from "./views/NotFoundPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -12,9 +13,10 @@ function App() {
     <Router>
       <Main />
       <Switch>
-      	<Route path="/" exact component={HomePage}/>
-      	<Route path="/forms" exact component={FormPage}/>
-      	<Route exact component={NotFoundPage}/>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/forms" exact component={FormPage} />
+        <Route path="/patients" exact component={PatientPage} />
+        <Route exact component={NotFoundPage} />
       </Switch>
     </Router>
   );
