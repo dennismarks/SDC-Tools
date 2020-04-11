80051ac5472cccbc7625ba579df95c7dd0f66d32
Max Pham <max.pham@mail.utoronto.ca>
2020-04-11 19:41:32 -0400

    refactoring

--- backend/routes/form.js
+++ backend/routes/form.js
@@ -17,19 +17,21 @@ router.route("/import").post((req, res) => {
     if (!e) {
       parser.xmlParse(file).then((data) => {
         // upload to Atlas
-        form.collection
-          .insertOne(data)
-          .then((x) => {
-            admin.findOneAndUpdate({}, { new: true }, (err, re) => {
-              re.allForms.push({
-                formID: data.formID,
-                formTitle: data.formTitle,
-              });
-              re.save();
-              res.status(200).send(re["allForms"]);
+        form.collection.insertOne(data).then((x) => {
+          admin.findOneAndUpdate({}, { new: true }, (err, re) => {
+            re.allForms.push({
+              formID: data.formID,
+              formTitle: data.formTitle,
             });
-          })
-          .catch((e) => res.status(409).send(e.message));
+            re.save()
+              .then((x) =>
+                res
+                  .status(200)
+                  .send(`inserted ${data.formID} - ${data.formTitle}`)
+              )
+              .catch((e) => res.status(409).send(e.message));
+          });
+        });
       });
     } else {
       res.status(500).send(e);
@@ -39,7 +41,10 @@ router.route("/import").post((req, res) => {
 
 router.route("/remove/:formID").delete((req, res) => {
   form.deleteOne({ formID: req.params.formID }).then((re) => {
-    res.status(200).send(`removed form - ${req.params.formID}`);
+    res
+      .status(200)
+      .send(`removed form - ${req.params.formID}`)
+      .catch((e) => res.status(500).send(e.message));
   });
 });
 
@@ -71,20 +76,17 @@ function cryptDe(encrypted) {
 
 // Get draft form specifically @formID @patientID
 router.route("/GET/:formID/:patientID").get((req, res) => {
-  form
-    .findOne({ formID: req.params.formID })
-    .then((data) => {
-      cryptEn(
-        req.params.formID.concat(data.version, req.params.patientID)
-      ).then((diag) => {
+  form.findOne({ formID: req.params.formID }).then((data) => {
+    cryptEn(req.params.formID.concat(data.version, req.params.patientID))
+      .then((diag) => {
         const newDraft = data.toObject();
         newDraft.diagnosticID = diag; // Generating diagnosticID for new Draft
         res.json(newDraft);
+      })
+      .catch((error) => {
+        res.status(404).send(error.message);
       });
-    })
-    .catch((error) => {
-      res.status(404).send(error.message);
-    });
+  });
 });
 
 // Get fillable form specifically @formID
@@ -100,33 +102,31 @@ router.route("/GET/:formID").get((req, res) => {
 });
 
 router.route("/save").post((req, res) => {
-  draft.collection
-    .insertOne(req.body.payload)
-    .then((data) => {
-      if (data.insertedCount !== 1) {
-        res.status(404).send("Unable to insert document to database");
-      }
+  draft.collection.insertOne(req.body.payload).then((data) => {
+    if (data.insertedCount !== 1) {
+      res.status(404).send("Unable to insert document to database");
+    }
 
-      cryptDe(req.body.payload.diagnosticID).then((decrypted) => {
-        // Append to patient profile
-        const patientID = decrypted.slice(-64);
-        patient.findOneAndUpdate(
-          { patientID: patientID },
-          { new: true },
-          (err, re) => {
-            re.relatedForms.push({
-              filler: null,
-              diagnosticID: req.body.payload.diagnosticID,
+    cryptDe(req.body.payload.diagnosticID).then((decrypted) => {
+      // Append to patient profile
+      const patientID = decrypted.slice(-64);
+      patient.findOneAndUpdate(
+        { patientID: patientID },
+        { new: true },
+        (err, re) => {
+          re.relatedForms.push({
+            filler: null,
+            diagnosticID: req.body.payload.diagnosticID,
+          });
+          re.save()
+            .then((x) => res.status(200).send("added draft to patient profile"))
+            .catch((error) => {
+              res.status(500).send(error.message);
             });
-            re.save();
-            res.status(200).send("added draft to patient profile");
-          }
-        );
-      });
-    })
-    .catch((error) => {
-      res.status(404).send(error.message);
+        }
+      );
     });
+  });
 });
 
 module.exports = router;
