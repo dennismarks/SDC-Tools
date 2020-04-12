655f8feef6f78e3856cdf1b1ef652191389320e2
Michael Katilevsky <katilevsky.michael@gmail.com>
2020-04-12 17:08:24 -0400

    Fix CI test failures

--- frontend/src/components/SDCQuestion.jsx
+++ frontend/src/components/SDCQuestion.jsx
@@ -35,16 +35,16 @@ export default class SDCQuestion extends Component {
       answer: props.question.answerObject.answer
     }
 
-    if (props.question.answerType.$numberInt == 4) {
+    if (props.question.answerType.$numberInt === 4) {
       this.state["is_true_false"] = true;
     }
-    else if (props.question.answerType.$numberInt == 3) {
+    else if (props.question.answerType.$numberInt === 3) {
       this.state["is_multiple_choice_checkbox"] = true;
     }
-    else if (props.question.answerType.$numberInt == 2) {
+    else if (props.question.answerType.$numberInt === 2) {
       this.state["is_multiple_choice_radio"] = true;
     }
-    else if (props.question.answerType.$numberInt == 1) {
+    else if (props.question.answerType.$numberInt === 1) {
       this.state["is_int"] = true;
     }
     else {
