011c8e631308a64885d7459185316ded4bb49289
Michael Katilevsky <katilevsky.michael@gmail.com>
2020-04-12 17:28:40 -0400

    Fixed another CI failure

--- frontend/src/components/MultipleChoiceCheckboxQuestionBody.jsx
+++ frontend/src/components/MultipleChoiceCheckboxQuestionBody.jsx
@@ -8,18 +8,16 @@ export default class MultipleChoiceCheckboxQuestionBody extends Component {
     this.render = this.render.bind(this);
     this.handleChange = this.handleChange.bind(this);
 
-    this.state = {
-      checked_options: {}
-    }
+    this.state = {}
 
     if (props.answer) {
       for (let i = 0; i < props.question_options.length; i++) {
         let option = props.question_options[i];
         if (props.answer.indexOf(option.optionID) > -1) {
-          this.state["checked_options"][option.optionID] = true;
+          this.state[option.optionID] = true;
         }
         else {
-          this.state["checked_options"][option.optionID] = false;
+          this.state[option.optionID] = false;
         }
       }
     }
@@ -27,12 +25,14 @@ export default class MultipleChoiceCheckboxQuestionBody extends Component {
 
   handleChange(event) {
     let value = event.target.value;
-    this.state["checked_options"][value] = !this.state["checked_options"][value]
+    let newState = {};
+    newState[value] = !this.state[value];
+    this.setState(newState);
 
     let answer = [];
     for (let i = 0; i < this.props.question_options.length; i++) {
       let option = this.props.question_options[i];
-      if (this.state["checked_options"][option.optionID]) {
+      if (this.state[option.optionID]) {
         answer.push(option.optionID);
       }
     }
@@ -45,7 +45,7 @@ export default class MultipleChoiceCheckboxQuestionBody extends Component {
       let option = this.props.question_options[i];
       components.push(
         <div style={{marginBottom: "3px"}}>
-          <input type="checkbox" id={this.props.question_id + option.optionID} name={this.props.question_id} value={option.optionID} onChange={this.handleChange} checked={this.state["checked_options"][option.optionID]} />
+          <input type="checkbox" id={this.props.question_id + option.optionID} name={this.props.question_id} value={option.optionID} onChange={this.handleChange} checked={this.state[option.optionID]} />
           <label style={{paddingLeft: "3px"}} for={this.props.question_id + option.optionID}>{option.value}</label>
         </div>
       );
