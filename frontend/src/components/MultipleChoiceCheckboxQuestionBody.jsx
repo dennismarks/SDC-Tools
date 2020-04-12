6cfc3840c3c7ef2388dcfc8acd5c6ddd4172d201
Michael Katilevsky <katilevsky.michael@gmail.com>
2020-04-12 16:02:02 -0400

    Updated multiple choice question components to reflect new schema

--- frontend/src/components/MultipleChoiceCheckboxQuestionBody.jsx
+++ frontend/src/components/MultipleChoiceCheckboxQuestionBody.jsx
@@ -15,11 +15,11 @@ export default class MultipleChoiceCheckboxQuestionBody extends Component {
     if (props.answer) {
       for (let i = 0; i < props.question_options.length; i++) {
         let option = props.question_options[i];
-        if (props.answer.indexOf(option["id"]) > -1) {
-          this.state["checked_options"][option["id"]] = true;
+        if (props.answer.indexOf(option.optionID) > -1) {
+          this.state["checked_options"][option.optionID] = true;
         }
         else {
-          this.state["checked_options"][option["id"]] = false;
+          this.state["checked_options"][option.optionID] = false;
         }
       }
     }
@@ -32,8 +32,8 @@ export default class MultipleChoiceCheckboxQuestionBody extends Component {
     let answer = [];
     for (let i = 0; i < this.props.question_options.length; i++) {
       let option = this.props.question_options[i];
-      if (this.state["checked_options"][option["id"]]) {
-        answer.push(option["id"]);
+      if (this.state["checked_options"][option.optionID]) {
+        answer.push(option.optionID);
       }
     }
     this.props.onChange(answer);
@@ -45,8 +45,8 @@ export default class MultipleChoiceCheckboxQuestionBody extends Component {
       let option = this.props.question_options[i];
       components.push(
         <div style={{marginBottom: "3px"}}>
-          <input type="checkbox" id={this.props.question_id + option["id"]} name={this.props.question_id} value={option["id"]} onChange={this.handleChange} checked={this.state["checked_options"][option["id"]]} />
-          <label style={{paddingLeft: "3px"}} for={this.props.question_id + option["id"]}>{option["value"]}</label>
+          <input type="checkbox" id={this.props.question_id + option.optionID} name={this.props.question_id} value={option.optionID} onChange={this.handleChange} checked={this.state["checked_options"][option.optionID]} />
+          <label style={{paddingLeft: "3px"}} for={this.props.question_id + option.optionID}>{option.value}</label>
         </div>
       );
     }
