22875ab7c53e37549f7445cd062e4df690f348c7
Dennis M <dennismarkshome@gmail.com>
2020-04-12 17:25:40 -0400

    Update Main Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Main from "./components/Main"
+import Main from "./components/Main";
 import AboutPage from "./views/AboutPage";
 import ContactPage from "./views/ContactPage";
 import DraftPage from "./views/DraftPage";
 import FormPage from "./views/FormPage";
-import HomePage from "./views/HomePage";
+// import HomePage from "./views/HomePage"; // old home page
+import MainPage from "./views/MainPage"; // new home page
 import PatientPage from "./views/PatientPage";
 import NotFoundPage from "./views/NotFoundPage";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,13 +17,14 @@ function App() {
     <Router>
       <Main />
       <Switch>
-      	<Route path="/" exact component={HomePage}/>
-      	<Route path="/forms" component={FormPage}/>
-      	<Route path="/draft/:form_id/:patient_id" component={DraftPage}/>
-        <Route path="/patients" component={PatientPage}/>
-        <Route path="/about" component={AboutPage}/>
-        <Route path="/contact" component={ContactPage}/>
-      	<Route path="*" component={NotFoundPage}/>
+        {/* <Route path="/" exact component={HomePage}/> */}
+        <Route path="/" exact component={MainPage} />
+        <Route path="/forms" component={FormPage} />
+        <Route path="/draft/:form_id/:patient_id" component={DraftPage} />
+        <Route path="/patients" component={PatientPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
+        <Route path="*" component={NotFoundPage} />
       </Switch>
     </Router>
   );
