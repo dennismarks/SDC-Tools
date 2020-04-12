7dbe1b8269c7e51f08ace588e1ab868720f566f6
Michael Katilevsky <katilevsky.michael@gmail.com>
2020-04-12 16:03:43 -0400

    Finished SDC Form rendering

--- frontend/src/views/DraftPage.jsx
+++ frontend/src/views/DraftPage.jsx
@@ -1,23 +1,45 @@
 import React, { Component } from "react";
+import SDCSection from "../components/SDCSection"
 
 export default class DraftPage extends Component {
 	constructor(props) {
 		super(props);
-		console.log(this.props.match.params)
+
+		this.handleChange = this.handleChange.bind(this);
+
+		this.state = {
+			form_json: {} // TODO: Get the form JSON, either through the request body, component props, or API call 
+		}
+	}
+
+	handleChange(value, questionID) {
+		console.log(questionID + ":\t" + value);
 	}
 
 	render() {
+
+		let sections = []
+
+		this.state.form_json.sections.forEach((section) => {
+		 sections.push(
+			 <SDCSection section={section} onChange={this.handleChange}/>
+		 );
+		});
+
+		const formStyle = {
+		 marginTop: "80px",
+		 marginLeft: "5%",
+		 marginRight: "5%",
+		 maringBottom: "30px",
+		 backgroundColor: "#282c34"
+		}
+
 		return (
 			<div className="draft-page">
 				<div className="App-header">
-					<h1>
-						DraftPage
-					</h1>
-					<p>
-						Patient: {this.props.match.params.patient_id}
-						<br/>
-						Form: {this.props.match.params.form_id}
-					</p>
+					<div style={formStyle}>
+						{sections}
+					</div>
 				</div>
 			</div>
 		);
