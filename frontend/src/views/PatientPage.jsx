702e6205b632b5dd77a14b36019177ae2163d918
RUNCHAO MAO <rcmao97@gmail.com>
2020-04-14 02:23:50 -0400

    add go back button so that user can go between table 1 and table 2

--- frontend/src/views/PatientPage.jsx
+++ frontend/src/views/PatientPage.jsx
@@ -77,102 +77,94 @@ export default class PatientPage extends Component {
                 <div className="App-header">
 
 
-                <div>
-                <h1>Enter Patient Name:</h1>
-                <Form onSubmit={this.retrieveAllPatients}>
-                    <Form.Row>
-                        <Col>
-                        <Form.Group>
-                        <Form.Label>Patient Name:</Form.Label>
-                        <Form.Control
-                        value={this.state.name}
-                        onChange={this.enterPatientName}
-                        />
-                        </Form.Group>
-                        </Col>
-                        <Col>
-                        <Button
-                        variant="primary"
-                        type="submit"
-                        style={{ marginTop: "32px", marginLeft: "6px" }}
-                        >
-                        Submit
-                        </Button>
-                        </Col>
-                    </Form.Row>     
-                </Form>
-                </div>
+                    <div>
+                    <h1>Enter Patient Name:</h1>
+                    <Form onSubmit={this.retrieveAllPatients}>
+                        <Form.Row>
+                            <Col>
+                            <Form.Group>
+                            <Form.Label>Patient Name:</Form.Label>
+                            <Form.Control
+                            value={this.state.name}
+                            onChange={this.enterPatientName}
+                            />
+                            </Form.Group>
+                            </Col>
+                            <Col>
+                            <Button
+                            variant="primary"
+                            type="submit"
+                            style={{ marginTop: "32px", marginLeft: "6px" }}
+                            >
+                            Submit
+                            </Button>
+                            </Col>
+                        </Form.Row>     
+                    </Form>
+                    </div>
 
-                <div>
-                <div style={{float: 'left'}}><Image src={goBack} style={goBackStyling} onClick={this.goBackToPreviousTable}/></div>
+                    <div>
+                        <div style={{float: 'left'}}><Image src={goBack} style={goBackStyling} onClick={this.goBackToPreviousTable}/></div>
 
-<div style={{float: 'left'}}>
-<Table
-striped
-bordered
-hover
-variant="dark"
-style={{ maxWidth: "max-content" }}>
-    <div style={{display : this.state.showTr_1 ? 'block':'none'}}>
-    <thead >
-        <tr >
-        <th>Name</th>
-        <th>Email</th>
-        <th>Phone #</th>
-        <th>Choose</th>
-        </tr>
-    </thead>
-    
-    <tbody>
-        {this.state.allPatients.map(patient => (
-            <tr>
-            <td>{patient.name}</td>
-            <td>{patient.email}</td>
-            <td>{patient.phone}</td>
-            <td>
-                <Button onClick={() => this.retireveAllRelatedForms(patient.patientID)}>Choose</Button>
-            </td>
-            </tr>
-        ))}
-    </tbody>
-    </div>
-</Table>
+                        <div style={{float: 'left'}}>
+                            <Table
+                            striped
+                            bordered
+                            hover
+                            variant="dark"
+                            style={{ maxWidth: "max-content" }}>
+                                <div style={{display : this.state.showTr_1 ? 'block':'none'}}>
+                                <thead >
+                                    <tr >
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Phone #</th>
+                                    <th>Choose</th>
+                                    </tr>
+                                </thead>
+                                
+                                <tbody>
+                                    {this.state.allPatients.map(patient => (
+                                        <tr>
+                                        <td>{patient.name}</td>
+                                        <td>{patient.email}</td>
+                                        <td>{patient.phone}</td>
+                                        <td>
+                                            <Button onClick={() => this.retireveAllRelatedForms(patient.patientID)}>Choose</Button>
+                                        </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                                </div>
+                            </Table>
 
-<Table
-striped
-bordered
-hover
-variant="dark"
-style={{ maxWidth: "max-content" }}>
-    <div style={{display : this.state.showTr_2 ? 'block':'none'}}>
-    <thead>
-        <tr>
-        <th>filler</th>
-        <th>link</th>
-        </tr>
-    </thead>
-    <tbody>
-        {this.state.relatedForms.map(formObject => (
-            <tr>
-            <td>{formObject.filler}</td>
-            <td>
-                <Button>Link</Button>
-            </td>
-            </tr>
-        ))}
-    </tbody>
-    </div>
-</Table>
-</div>
-
-                </div>
-
-                
-                
-
-
-
-                
+                            <Table
+                            striped
+                            bordered
+                            hover
+                            variant="dark"
+                            style={{ maxWidth: "max-content" }}>
+                                <div style={{display : this.state.showTr_2 ? 'block':'none'}}>
+                                <thead>
+                                    <tr>
+                                    <th>filler</th>
+                                    <th>link</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.state.relatedForms.map(formObject => (
+                                        <tr>
+                                        <td>{formObject.filler}</td>
+                                        <td>
+                                            <Button>Link</Button>
+                                        </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                                </div>
+                            </Table>
+                        </div>
+                    </div>
                 </div>                     
             </div>
         );
