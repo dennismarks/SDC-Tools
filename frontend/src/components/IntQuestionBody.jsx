7200960feee2ebcc17275377ae3b979f79ecadb7
Michael Katilevsky <katilevsky.michael@gmail.com>
2020-04-15 00:37:33 -0400

    Add moreInfo functionality to frontend components

--- frontend/src/components/IntQuestionBody.jsx
+++ frontend/src/components/IntQuestionBody.jsx
@@ -7,18 +7,27 @@ export default class TextQuestionBody extends Component {
 
     this.render = this.render.bind(this);
     this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      answer: props.answer
+    };
   }
 
   handleChange(event) {
     const text = event.target.value;
     const number = parseInt(text);
+
+    this.setState({
+      answer: number
+    });
+
     this.props.onChange(number);
   }
 
   render() {
     return (
       <div>
-        <input style={{paddingLeft: "3px"}} type="number" id={this.props.question_id} onChange={this.handleChange} value={this.props.answer} />
+        <input style={{paddingLeft: "3px"}} type="number" id={this.props.question_id} onChange={this.handleChange} value={this.state.answer} />
         {this.props.children}
       </div>
     );
--- frontend/src/components/TextQuestionBody.jsx
+++ frontend/src/components/TextQuestionBody.jsx
@@ -7,17 +7,26 @@ export default class TextQuestionBody extends Component {
 
     this.render = this.render.bind(this);
     this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      answer: props.answer
+    };
   }
 
   handleChange(event) {
     const text = event.target.value;
+
+    this.setState({
+      answer: text
+    });
+
     this.props.onChange(text);
   }
 
   render() {
     return (
       <div>
-        <input style={{width: "100%", paddingLeft: "3px"}} type="text" id={this.props.question_id} onChange={this.handleChange} value={this.props.answer} />
+        <input style={{width: "100%", paddingLeft: "3px"}} type="text" id={this.props.question_id} onChange={this.handleChange} value={this.state.answer} />
         {this.props.children}
       </div>
     );
